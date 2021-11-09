0232f2ed7307b68af7a04f009d4f8b13f83d38c5
gio <jojuagio7@gmail.com>
2021-11-09 18:55:21 +0400

    XO Start

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+// 'use strict'
+//
+// let arrayXO = document.getElementsByClassName('box')
+//
+// for (let i = 0; i < arrayXO.length; i++) {
+//     // if (i % 2 === 1) {
+//     // } else if (i % 2 === 0) {
+//     //
+//     // }
+//     console.log('div')
+// }
+// console.log(arrayXO)
---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+
+mix.setPublicPath('public');
+mix.setResourceRoot('/');
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/scss/main.scss', 'public/css')
+    .copy('resources/views/*.html', 'public')
+    // .copy('resources/images/**', 'public/images/')
