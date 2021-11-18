6c953d2c3dfa28a0db0685ce9875cb0e25fcc2a2
gio <jojuagio7@gmail.com>
2021-11-18 18:33:05 +0400

    XO Game

--- public/js/app.js
+++ public/js/app.js
@@ -24,18 +24,143 @@ __webpack_require__.r(__webpack_exports__);
   \********************************/
 /***/ (() => {
 
-// 'use strict'
-//
-// let arrayXO = document.getElementsByClassName('box')
-//
-// for (let i = 0; i < arrayXO.length; i++) {
-//     // if (i % 2 === 1) {
-//     // } else if (i % 2 === 0) {
-//     //
-//     // }
-//     console.log('div')
-// }
-// console.log(arrayXO)
+"use strict";
+
+
+var arrayXO = document.querySelectorAll('.box');
+var clicks = 0;
+
+var _loop = function _loop(i) {
+  arrayXO[i].addEventListener('click', function () {
+    if (clicks === 0 || clicks % 2 === 0 && arrayXO[i].innerHTML == 0) {
+      clicks++;
+      arrayXO[i].innerHTML = 'X';
+      checkWinX();
+    } else if (arrayXO[i].innerHTML == 0) {
+      clicks++;
+      arrayXO[i].innerHTML = 'O';
+      checkWinO();
+    }
+
+    event.preventDefault();
+  });
+};
+
+for (var i = 0; i < arrayXO.length; i++) {
+  _loop(i);
+}
+
+var restartModal = document.getElementById('win');
+var restartModalText = document.getElementById('textWin');
+var restartXO = document.getElementById('restartXO');
+var resetXO = document.getElementById('resetXO');
+
+resetXO.onclick = function () {
+  clicks = 0;
+
+  for (var _i = 0; _i < arrayXO.length; _i++) {
+    arrayXO[_i].innerHTML = '';
+  }
+};
+
+console.log(restartXO);
+
+restartXO.onclick = function () {
+  clicks = 0;
+  restartModal.style.display = 'none';
+
+  for (var _i2 = 0; _i2 < arrayXO.length; _i2++) {
+    arrayXO[_i2].innerHTML = '';
+  }
+};
+
+function checkWinX() {
+  switch (true) {
+    case arrayXO[0].innerHTML == 'X' && arrayXO[1].innerHTML == 'X' && arrayXO[2].innerHTML == 'X':
+      restartModalText.innerHTML = "<h2>X Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[3].innerHTML == 'X' && arrayXO[4].innerHTML == 'X' && arrayXO[5].innerHTML == 'X':
+      restartModalText.innerHTML = "<h2>X Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[6].innerHTML == 'X' && arrayXO[7].innerHTML == 'X' && arrayXO[8].innerHTML == 'X':
+      restartModalText.innerHTML = "<h2>X Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[0].innerHTML == 'X' && arrayXO[3].innerHTML == 'X' && arrayXO[6].innerHTML == 'X':
+      restartModalText.innerHTML = "<h2>X Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[1].innerHTML == 'X' && arrayXO[4].innerHTML == 'X' && arrayXO[7].innerHTML == 'X':
+      restartModalText.innerHTML = "<h2>X Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[2].innerHTML == 'X' && arrayXO[5].innerHTML == 'X' && arrayXO[8].innerHTML == 'X':
+      restartModalText.innerHTML = "<h2>X Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[0].innerHTML == 'X' && arrayXO[4].innerHTML == 'X' && arrayXO[8].innerHTML == 'X':
+      restartModalText.innerHTML = "<h2>X Wins</h2>  ";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[2].innerHTML == 'X' && arrayXO[4].innerHTML == 'X' && arrayXO[6].innerHTML == 'X':
+      restartModalText.innerHTML = "<h2>X Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+  }
+}
+
+function checkWinO() {
+  switch (true) {
+    case arrayXO[0].innerHTML == 'O' && arrayXO[1].innerHTML == 'O' && arrayXO[2].innerHTML == 'O':
+      restartModalText.innerHTML = "<h2>O Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[3].innerHTML == 'O' && arrayXO[4].innerHTML == 'O' && arrayXO[5].innerHTML == 'O':
+      restartModalText.innerHTML = "<h2>O Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[6].innerHTML == 'O' && arrayXO[7].innerHTML == 'O' && arrayXO[8].innerHTML == 'O':
+      restartModalText.innerHTML = "<h2>O Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[0].innerHTML == 'O' && arrayXO[3].innerHTML == 'O' && arrayXO[6].innerHTML == 'O':
+      restartModalText.innerHTML = "<h2>O Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[1].innerHTML == 'O' && arrayXO[4].innerHTML == 'O' && arrayXO[7].innerHTML == 'O':
+      restartModalText.innerHTML = "<h2>O Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[2].innerHTML == 'O' && arrayXO[5].innerHTML == 'O' && arrayXO[8].innerHTML == 'O':
+      restartModalText.innerHTML = "<h2>O Wins</h2>  ";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[0].innerHTML == 'O' && arrayXO[4].innerHTML == 'O' && arrayXO[8].innerHTML == 'O':
+      restartModalText.innerHTML = "<h2>O Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+
+    case arrayXO[2].innerHTML == 'O' && arrayXO[4].innerHTML == 'O' && arrayXO[6].innerHTML == 'O':
+      restartModalText.innerHTML = "<h2>O Wins</h2>";
+      restartModal.style.display = 'flex';
+      break;
+  }
+}
 
 /***/ }),
 
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,116 @@
-// 'use strict'
-//
-// let arrayXO = document.getElementsByClassName('box')
-//
-// for (let i = 0; i < arrayXO.length; i++) {
-//     // if (i % 2 === 1) {
-//     // } else if (i % 2 === 0) {
-//     //
-//     // }
-//     console.log('div')
-// }
-// console.log(arrayXO)
+'use strict'
+
+let arrayXO = document.querySelectorAll('.box')
+let clicks = 0
+
+for (let i = 0; i < arrayXO.length; i++) {
+    arrayXO[i].addEventListener('click', function () {
+        if (clicks === 0 || clicks % 2 === 0 && arrayXO[i].innerHTML == 0) {
+            clicks++
+            arrayXO[i].innerHTML = 'X'
+            checkWinX()
+        } else if (arrayXO[i].innerHTML == 0) {
+            clicks++
+            arrayXO[i].innerHTML = 'O'
+            checkWinO()
+        }
+        event.preventDefault()
+    })
+}
+
+let restartModal = document.getElementById('win')
+let restartModalText = document.getElementById('textWin')
+let restartXO = document.getElementById('restartXO')
+let resetXO = document.getElementById('resetXO')
+
+resetXO.onclick = function () {
+    clicks = 0
+    for (let i = 0; i < arrayXO.length; i++) {
+        arrayXO[i].innerHTML = ''
+    }
+}
+
+console.log(restartXO)
+
+restartXO.onclick = function () {
+    clicks = 0
+    restartModal.style.display = 'none'
+    for (let i = 0; i < arrayXO.length; i++) {
+        arrayXO[i].innerHTML = ''
+    }
+}
+
+function checkWinX() {
+    switch (true) {
+        case arrayXO[0].innerHTML == 'X' && arrayXO[1].innerHTML == 'X' && arrayXO[2].innerHTML == 'X':
+            restartModalText.innerHTML = `<h2>X Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[3].innerHTML == 'X' && arrayXO[4].innerHTML == 'X' && arrayXO[5].innerHTML == 'X':
+            restartModalText.innerHTML = `<h2>X Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[6].innerHTML == 'X' && arrayXO[7].innerHTML == 'X' && arrayXO[8].innerHTML == 'X':
+            restartModalText.innerHTML = `<h2>X Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[0].innerHTML == 'X' && arrayXO[3].innerHTML == 'X' && arrayXO[6].innerHTML == 'X':
+            restartModalText.innerHTML = `<h2>X Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[1].innerHTML == 'X' && arrayXO[4].innerHTML == 'X' && arrayXO[7].innerHTML == 'X':
+            restartModalText.innerHTML = `<h2>X Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[2].innerHTML == 'X' && arrayXO[5].innerHTML == 'X' && arrayXO[8].innerHTML == 'X':
+            restartModalText.innerHTML = `<h2>X Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[0].innerHTML == 'X' && arrayXO[4].innerHTML == 'X' && arrayXO[8].innerHTML == 'X':
+            restartModalText.innerHTML = `<h2>X Wins</h2>  `
+            restartModal.style.display = 'flex'
+            break
+        case arrayXO[2].innerHTML == 'X' && arrayXO[4].innerHTML == 'X' && arrayXO[6].innerHTML == 'X':
+            restartModalText.innerHTML = `<h2>X Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+    }
+}
+
+function checkWinO() {
+    switch (true) {
+        case arrayXO[0].innerHTML == 'O' && arrayXO[1].innerHTML == 'O' && arrayXO[2].innerHTML == 'O':
+            restartModalText.innerHTML = `<h2>O Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[3].innerHTML == 'O' && arrayXO[4].innerHTML == 'O' && arrayXO[5].innerHTML == 'O':
+            restartModalText.innerHTML = `<h2>O Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[6].innerHTML == 'O' && arrayXO[7].innerHTML == 'O' && arrayXO[8].innerHTML == 'O':
+            restartModalText.innerHTML = `<h2>O Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[0].innerHTML == 'O' && arrayXO[3].innerHTML == 'O' && arrayXO[6].innerHTML == 'O':
+            restartModalText.innerHTML = `<h2>O Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[1].innerHTML == 'O' && arrayXO[4].innerHTML == 'O' && arrayXO[7].innerHTML == 'O':
+            restartModalText.innerHTML = `<h2>O Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[2].innerHTML == 'O' && arrayXO[5].innerHTML == 'O' && arrayXO[8].innerHTML == 'O':
+            restartModalText.innerHTML = `<h2>O Wins</h2>  `
+            restartModal.style.display = 'flex'
+            break;
+        case arrayXO[0].innerHTML == 'O' && arrayXO[4].innerHTML == 'O' && arrayXO[8].innerHTML == 'O':
+            restartModalText.innerHTML = `<h2>O Wins</h2>`
+            restartModal.style.display = 'flex'
+            break
+        case arrayXO[2].innerHTML == 'O' && arrayXO[4].innerHTML == 'O' && arrayXO[6].innerHTML == 'O':
+            restartModalText.innerHTML = `<h2>O Wins</h2>`
+            restartModal.style.display = 'flex'
+            break;
+    }
+}
+
